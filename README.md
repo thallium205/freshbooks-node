e7c25ad78bebbb64463202a0fb8d803a09b1c8ee
John Russell <johnrussells@gmail.com>
2013-04-11 09:14:05 -0700

    initial

--- README.md
+++ README.md
@@ -1,4 +1,44 @@
-freshbooks-node
-===============
+# freshbooks-node - A lightweight node.js library for Freshbooks
+## Install
+<pre>
+  npm install freshbooks-node
+</pre>
+## Usage
+### Request
+```javascript
+var Freshbooks = require('freshbooks-node');
+var freshbooks = new Freshbooks(APPLICATION_NAME, APPLICATION_TOKEN, USER_AGENT)
+freshbooks.call('invoice.create',
+    {
+        invoice:
+        {   client_id: 32,
+            status: 'draft',
+            lines: {
+                line:
+                {
+                    name: 'Tulips',
+                    description: 'A type of flower',
+                    unit_cost: 10,
+                    quantity: 2
+                }
+            }
+        }
+    }, function(err, json) {
+        if (err) {
+            console.error(err);
+        }
+        console.log(JSON.stringify(json));
+    });
+```
+### Response
+```javascript
+{
+    "response": {
+        "invoice_id": "73869"
+    }
+}
+```
+## Docs
+http://developers.freshbooks.com/
 
-A lightweight node.js library for Freshbooks
+Enjoy!
---
+++ index.js
@@ -0,0 +1,47 @@
+var request = require('request');
+var easyxml = require('Easyxml');
+var xml2js = require('xml2js');
+
+function Freshbooks(account, token, agent, showAttributes) {
+    this.url = 'https://' + account + '.freshbooks.com/api/2.1/xml-in';
+    this.token = token;
+    this.agent = agent;
+    this.parser = new xml2js.Parser({explicitArray: false, ignoreAttrs: showAttributes !== undefined ? (showAttributes ? false : true) : true, async: true});
+    easyxml.configure({rootElement: 'request', manifest: true});
+}
+
+Freshbooks.prototype.call = function(method, json, callback) {
+    var self = this;
+
+    var xml = easyxml.render(json);
+    xml = xml.replace('<request>', '<request method="' + method + '">');
+
+    var options = {
+        uri: self.url,
+        method: "POST",
+        headers: {
+            'Authorization': 'Basic ' + new Buffer(self.token + ':X').toString('base64'),
+            'User-Agent': self.agent
+        },
+        body: xml
+    }
+
+    request(options, function(err, res, body) {
+        if (err) {
+            return callback(err);
+        } else if (res.statusCode !== 200) {
+            return callback(new Error(res.statusCode));
+        } else {
+            self.parser.parseString(body, function(err, json) {
+                if (err) return callback(err);
+                if (json && json.response && json.response.error) {
+                    return callback(json);
+                } else {
+                    return callback(null, json);
+                }
+            });
+        }
+    });
+}
+
+module.exports = Freshbooks;
